0be03c4b2aac274a7839a064d2674dafc84882c6
PreetiDiwakarGitHub <p208016@gmail.com>
2025-03-03 09:40:55 +0530

    car

--- Script.js
+++ Script.js
@@ -4,7 +4,7 @@ const stopButton = document.getElementById("stop");
 const backButton = document.getElementById("back");
 
 let interval = null; 
-let speed = 9;
+let speed = 15;
 function moveCar(direction) {
     stopCar();
 
@@ -12,14 +12,10 @@ function moveCar(direction) {
         let currentLeft = parseInt(car.style.left) || 0; 
         if (direction === "forward") {
             currentLeft += speed;
-            if (currentLeft > window.innerWidth) { 
-                currentLeft = -car.offsetWidth;
-            }
+            
         } else if (direction === "backward") {
             currentLeft -= speed;
-            if (currentLeft < -car.offsetWidth) {
-                currentLeft = window.innerWidth;
-            }
+            
         }
 
         car.style.left = currentLeft + "px";
